abd23198a5600943d13a7af48479f4baa2aa63af
Chris Whong <chris@whong.org>
2017-09-09 20:57:44 -0400

    Fix lint error

--- app/utils/search-pluto-lots.js
+++ app/utils/search-pluto-lots.js
@@ -2,15 +2,15 @@ import carto from '../utils/carto';
 
 export default function searchPlutoLots(text) {
   const SQL = `
-    SELECT (address || ', ' || 
-      CASE 
-        WHEN borough = 'MN' THEN 'Manhattan' 
-        WHEN borough = 'BX' THEN 'Bronx' 
-        WHEN borough = 'BK' THEN 'Brooklyn' 
-        WHEN borough = 'QN' THEN 'Queens' 
+    SELECT (address || ', ' ||
+      CASE
+        WHEN borough = 'MN' THEN 'Manhattan'
+        WHEN borough = 'BX' THEN 'Bronx'
+        WHEN borough = 'BK' THEN 'Brooklyn'
+        WHEN borough = 'QN' THEN 'Queens'
         WHEN borough = 'SI' THEN 'Staten Island'
-      END) as address, bbl FROM support_mappluto 
+      END) as address, bbl FROM support_mappluto
      WHERE address LIKE '%25${text.toUpperCase()}%25' LIMIT 10`;
 
   return carto.SQL(SQL);
-};
+}
