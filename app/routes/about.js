0a3c3bda480ab3f392f55d5329d12f389a0d79ab
Andy Cochran <acochran@planning.nyc.gov>
2017-09-19 16:23:29 -0400

    nix cruft in about.js breaking tests

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
-const { service } = Ember.inject;
-
 export default Ember.Route.extend({
-  mainMap: service(),
-
-  afterModel() {
-    this.get('mainMap').resetBounds();
-  },
 });
