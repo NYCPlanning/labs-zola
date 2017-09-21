26bc0a32570469c099231c3f3510a48a7cd22b26
allthesignals <wilburnforce@gmail.com>
2017-09-20 22:20:56 -0400

    Make lot the cell; remove jquery;
    use an auto sizing cell: http://foundation.zurb.com/sites/docs/xy-grid.html#auto-sizing

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,14 +5,6 @@ const { service } = Ember.inject;
 export default Ember.Route.extend({
   mainMap: service(),
 
-  activate() {
-    Ember.$('body').toggleClass('content-is-closed');
-  },
-
-  deactivate() {
-    Ember.$('body').toggleClass('content-is-closed');
-  },
-
   afterModel() {
     this.get('mainMap').resetBounds();
   },
