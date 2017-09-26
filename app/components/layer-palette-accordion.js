39cc82027b245c1baecb0634ae5895afa58a4816
allthesignals <wilburnforce@gmail.com>
2017-09-26 17:37:50 -0400

    Compute # of layers on

--- app/components/layer-palette-accordion.js
+++ app/components/layer-palette-accordion.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import { ParentMixin } from 'ember-composability-tools';
+import { computed } from 'ember-decorators/object'; // eslint-disable-line
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ParentMixin, {
   classNames: ['layer-palette-accordion'],
   closed: true,
+
+  didInsertElement() {
+    this.set('numberVisible', Ember.computed('childComponents.@each.visible', function() {
+      const childComponents = this.get('childComponents');
+      return childComponents.filterBy('visible', true).length;
+    }));
+  },
 });
