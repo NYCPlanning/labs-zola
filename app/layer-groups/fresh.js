1288b8c77cb01fd261ba521cf10416624c877bbc
Chris Whong <chris@whong.org>
2017-09-27 00:35:00 -0400

    Add sample titleTooltips

--- app/layer-groups/fresh.js
+++ app/layer-groups/fresh.js
@@ -1,6 +1,7 @@
 export default {
   id: 'fresh',
   title: 'FRESH Zones',
+  titleTooltip: 'FRESH promotes the establishment and expansion of neighborhood grocery stores in underserved communities by providing zoning and financial incentives to eligible grocery store operators and developers.',
   visible: false,
   type: 'carto',
   sql: ['SELECT the_geom_webmercator, name FROM support_fresh'],
