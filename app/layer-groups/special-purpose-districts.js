c9a3ebb6f6544fb476991d64242263a0239c3eb9
Chris Whong <chris@whong.org>
2017-09-30 22:36:40 -0400

    style + tooltips for special purpose districts and subdistricts

--- app/layer-groups/special-purpose-districts.js
+++ app/layer-groups/special-purpose-districts.js
@@ -11,9 +11,30 @@ export default {
         'source-layer': 'special-purpose-districts',
         paint: {
           'line-width': 2,
-          'line-color': 'red',
+          'line-color': 'rgba(61, 66, 210, 1)',
+          'line-dasharray': [1, 2],
+        },
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'miter',
         },
       },
+    },
+
+    {
+      layer: {
+        id: 'zoning-sp-fill',
+        type: 'fill',
+        source: 'supporting-zoning',
+        'source-layer': 'special-purpose-districts',
+        paint: {
+          'fill-color': 'rgba(101, 124, 230, 1)',
+          'fill-opacity': 0.2,
+        },
+        layout: {},
+      },
+      highlightable: true,
+      tooltipTemplate: '{{sdname}}',
     },
   ],
 };
--- app/layer-groups/special-purpose-subdistricts.js
+++ app/layer-groups/special-purpose-subdistricts.js
@@ -9,11 +9,36 @@ export default {
         type: 'line',
         source: 'supporting-zoning',
         'source-layer': 'special-purpose-subdistricts',
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'miter',
+        },
         paint: {
           'line-width': 2,
-          'line-color': 'green',
+          'line-color': 'rgba(41, 138, 125, 1)',
+          'line-dasharray': [
+            1,
+            2,
+          ],
         },
       },
+    },
+    {
+      layer: {
+        id: 'zoning-sp-sd-fill',
+        type: 'fill',
+        source: 'supporting-zoning',
+        'source-layer': 'special-purpose-subdistricts',
+        paint: {
+          'fill-color': 'rgba(67, 156, 146, 1)',
+          'fill-opacity': 0.2,
+        },
+        layout: {
+          visibility: 'visible',
+        },
+      },
+      highlightable: true,
+      tooltipTemplate: '{{spname}}',
     },
   ],
 };
