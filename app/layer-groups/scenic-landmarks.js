c4b1cd3b493d116fa59c8377f74a49d25cf3ea06
Chris Whong <chris@whong.org>
2017-10-05 11:54:12 -0400

    update scenic-landmarks with tooltip and new style

--- app/layer-groups/scenic-landmarks.js
+++ app/layer-groups/scenic-landmarks.js
@@ -1,19 +1,21 @@
+import { lineStyle, fillStyle } from '../utils/polygon-layer-styles';
+
+const legendColor = 'purple';
+
 export default {
   id: 'scenic-landmarks',
   title: 'Scenic Landmarks',
   visible: false,
+  legendIcon: 'polygon',
+  legendColor,
   layers: [
     {
-      layer: {
-        id: 'sceniclandmarks-line',
-        type: 'line',
-        source: 'landmark-historic',
-        'source-layer': 'scenic-landmarks',
-        paint: {
-          'line-width': 2,
-          'line-color': 'purple',
-        },
-      },
+      layer: lineStyle('scenic-landmarks-line', 'landmark-historic', 'scenic-landmarks', legendColor),
+    },
+    {
+      layer: fillStyle('scenic-landmarks-fill', 'landmark-historic', 'scenic-landmarks', legendColor),
+      highlightable: true,
+      tooltipTemplate: '{{{scen_lm_na}}}',
     },
   ],
 };
