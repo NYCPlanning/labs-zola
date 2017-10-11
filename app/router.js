37d57d4c41019ed56741ea3ea0135d25d623108c
allthesignals <wilburnforce@gmail.com>
2017-10-11 13:28:46 -0400

    SPSD route, Close #272, Address #218
    Still need search for SPSD

--- app/router.js
+++ app/router.js
@@ -6,11 +6,12 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function () { // eslint-disable-line
+Router.map(function () {  // eslint-disable-line
   this.route('lot', { path: 'lot/:boro/:block/:lot' });
   this.route('zma', { path: 'zma/:ulurpno' });
   this.route('zoning-district', { path: 'zoning-district/:zonedist' });
   this.route('special-purpose-district', { path: 'special-purpose-district/:id' });
+  this.route('special-purpose-subdistricts', { path: 'special-purpose-subdistrict/:id' });
   this.route('about');
   this.route('bookmarks');
 });
