e47c962eb6d7404a4f417026a6fd5674b1e583f3
Andy Cochran <acochran@planning.nyc.gov>
2017-10-11 15:02:19 -0400

    FRESH: filter layers, style each, new FRESH legend

--- app/layer-groups/fresh.js
+++ app/layer-groups/fresh.js
@@ -1,20 +1,80 @@
-import { lineStyle, fillStyle } from '../utils/polygon-layer-styles';
-
-const legendColor = '#32EE5A';
+import { lineStyle } from '../utils/polygon-layer-styles';
 
 export default {
   id: 'fresh',
   title: 'FRESH Zones',
   titleTooltip: 'FRESH promotes the establishment and expansion of neighborhood grocery stores in underserved communities by providing zoning and financial incentives to eligible grocery store operators and developers.',
   visible: false,
-  legendIcon: 'polygon',
-  legendColor,
   layers: [
     {
-      layer: lineStyle('fresh-line', 'supporting-zoning', 'fresh', legendColor),
+      layer: lineStyle('fresh-line', 'supporting-zoning', 'fresh', '#30BF4E'),
     },
     {
-      layer: fillStyle('fresh-fill', 'supporting-zoning', 'fresh', legendColor),
+      layer: {
+        id: 'fresh-fill-zoning-and-tax',
+        type: 'fill',
+        source: 'supporting-zoning',
+        'source-layer': 'fresh',
+        paint: {
+          'fill-color': '#30BF4E',
+          'fill-opacity': 0.2,
+        },
+        filter: [
+          'all',
+          [
+            '==',
+            'name',
+            'Zoning and discretionary tax incentives',
+          ],
+        ],
+        layout: {},
+      },
+      highlightable: true,
+      tooltipTemplate: 'FRESH - {{{name}}}',
+    },
+    {
+      layer: {
+        id: 'fresh-fill-zoning',
+        type: 'fill',
+        source: 'supporting-zoning',
+        'source-layer': 'fresh',
+        paint: {
+          'fill-color': '#0B9390',
+          'fill-opacity': 0.2,
+        },
+        filter: [
+          'all',
+          [
+            '==',
+            'name',
+            'Zoning incentives',
+          ],
+        ],
+        layout: {},
+      },
+      highlightable: true,
+      tooltipTemplate: 'FRESH - {{{name}}}',
+    },
+    {
+      layer: {
+        id: 'fresh-fill-tax',
+        type: 'fill',
+        source: 'supporting-zoning',
+        'source-layer': 'fresh',
+        paint: {
+          'fill-color': '#8FE339',
+          'fill-opacity': 0.2,
+        },
+        filter: [
+          'all',
+          [
+            '==',
+            'name',
+            'Discretionary tax incentives',
+          ],
+        ],
+        layout: {},
+      },
       highlightable: true,
       tooltipTemplate: 'FRESH - {{{name}}}',
     },
