3cf576b1e7e709eb50ab64daaf7b8ecbbede9fee
Andy Cochran <acochran@planning.nyc.gov>
2017-10-20 11:09:39 -0400

    swap vector 2007 floodplain rasters for vector

--- app/sources/effective-flood-insurance-rate-2007.js
+++ app/sources/effective-flood-insurance-rate-2007.js
@@ -1,6 +1,10 @@
 export default {
   id: 'effective-flood-insurance-rate-2007',
-  type: 'raster',
-  tiles: ['https://tiles2.arcgis.com/tiles/GfwWNkhOj9bNBqoJ/arcgis/rest/services/Effective_Flood_Insurance_Rate_Maps_2007/MapServer/tile/{z}/{y}/{x}'],
-  tileSize: 256,
+  type: 'cartovector',
+  'source-layers': [
+    {
+      id: 'effective-flood-insurance-rate-2007',
+      sql: 'SELECT * FROM firms_100_year_floodplain_2007',
+    },
+  ],
 };
