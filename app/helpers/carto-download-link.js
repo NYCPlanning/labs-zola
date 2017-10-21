96dc7327146b8bbeda3b7589cf7c7812219ea665
allthesignals <wilburnforce@gmail.com>
2017-10-21 14:29:20 -0400

    Fix tests, ESLint

--- app/helpers/carto-download-link.js
+++ app/helpers/carto-download-link.js
@@ -3,7 +3,7 @@ import { buildSqlUrl } from '../utils/carto';
 
 export function cartoDownloadLink([table, identifier, ids, format]) {
   const query = `SELECT * FROM ${table} WHERE ${identifier} IN (${ids.join(',')})`;
-  return buildSqlUrl(query, format) + `&filename=${table}`;
+  return `${buildSqlUrl(query, format)}&filename=${table}`;
 }
 
 export default Ember.Helper.helper(cartoDownloadLink);
--- tests/integration/helpers/carto-download-link-test.js
+++ tests/integration/helpers/carto-download-link-test.js
@@ -8,10 +8,15 @@ moduleForComponent('carto-download-link', 'helper:carto-download-link', {
 
 // Replace this with your real tests.
 test('it renders', function(assert) {
-  this.set('inputValue', '1234');
+  this.setProperties({
+    table: 'support_mappluto',
+    identifier: 'bbl',
+    ids: [1014970028, 1015280036, 1015280038],
+    format: 'csv',
+  });
 
-  this.render(hbs`{{carto-download-link inputValue}}`);
+  this.render(hbs`{{carto-download-link table identifier ids format}}`);
 
-  assert.equal(this.$().text().trim(), '1234');
+  assert.equal(this.$().text().trim(), 'https://carto.planninglabs.nyc/user/data/api/v2/sql?q=SELECT * FROM support_mappluto WHERE bbl IN (1014970028,1015280036,1015280038)&format=csv&filename=support_mappluto');
 });
 
