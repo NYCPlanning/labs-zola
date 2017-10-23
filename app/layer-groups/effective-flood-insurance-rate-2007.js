a6a33e7b86b8aef6705351c3f0a1cb7b12ed8948
Chengying <ceciliaheny@gmail.com>
2017-10-23 14:05:48 -0400

    Add tooltip to flood zone layers

--- app/layer-groups/effective-flood-insurance-rate-2007.js
+++ app/layer-groups/effective-flood-insurance-rate-2007.js
@@ -1,6 +1,7 @@
 export default {
   id: 'effective-flood-insurance-rate-2007',
   title: 'Effective Flood Insurance Rate Maps 2007',
+  titleTooltip: 'V Zone: portion of the 1% annual chance floodplain subject to high velocity wave action (a breaking wave 3 feet high or larger); A Zone: A portion of the area subject to flooding from the 1% annual chance flood; Shaded X Zone: The area of moderate flood risk outside the regulatory 1% annual chance flood but within the limits of the 0.2% annual chance flood level (the 500-year floodplain). ',
   visible: false,
   layers: [
     {
