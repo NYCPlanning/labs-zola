76dd73d41e4b597399484e9e01993fbe57df5443
Chengying <ceciliaheny@gmail.com>
2017-11-20 13:06:26 -0500

    fix test

--- tests/unit/controllers/zoning-district-test.js
+++ tests/unit/controllers/zoning-district-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:zoning-district', 'Unit | Controller | zoning district', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:metrics'],
 });
 
 // Replace this with your real tests.
