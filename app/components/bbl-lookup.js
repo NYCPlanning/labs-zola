0048cce31285dce1e9c78e7dcb712bfcc4cfaa9d
Chengying <ceciliaheny@gmail.com>
2017-11-20 17:03:16 -0500

    first draft get the bbl number from sql query

--- app/components/bbl-lookup.js
+++ app/components/bbl-lookup.js
@@ -1,6 +1,36 @@
 import Ember from 'ember';
+import carto from '../utils/carto';
+
+
+// export function getUniqueOptionsFor([column, sql]) {
+//   const uniqueSQL = `select distinct(${column}) as option from (${sql}) a ORDER BY option ASC`;
+//   return carto.SQL(uniqueSQL)
+//     .then(response =>
+//       response.map(row =>
+//         row.option,
+//       ),
+//     );
+// }
+
+
 
 export default Ember.Component.extend({
   classNames: ['bbl-lookup hide-for-print'],
+
+  boro: '',
+  block: '',
+  lot: '',
+
   closed: false,
+  actions: {
+    checkBBL() {
+      const { boro, block, lot } = this.getProperties('boro', 'block', 'lot');
+      const uniqueSQL = `select bbl from support_mappluto where block= ${block} and lot = ${lot} and borocode = ${boro}`;
+      carto.SQL(uniqueSQL).then(response => {
+        console.log(response);
+        return response;
+      });
+      console.log('checkBBL', uniqueSQL);
+    },
+  },
 });
