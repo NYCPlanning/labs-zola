73a6cbf75705b8e97a624ab148a7f107448b3eb9
Andy Cochran <acochran@planning.nyc.gov>
2017-11-21 14:09:26 -0500

    make bbl-lookup test pass

--- tests/integration/components/bbl-lookup-test.js
+++ tests/integration/components/bbl-lookup-test.js
@@ -12,14 +12,5 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{bbl-lookup}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#bbl-lookup}}
-      template block text
-    {{/bbl-lookup}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'BBL Lookup');
 });
