b2f631d3fc9671fcd5000ab453153f9c0f1b21ef
Chris Whong <chris@whong.org>
2017-11-22 12:23:09 -0500

    hotfix update table names for special purpose district and subdistricts

--- app/adapters/special-purpose-subdistrict.js
+++ app/adapters/special-purpose-subdistrict.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import { buildSqlUrl } from '../utils/carto';
 
 const SQL = function(id) {
-  return `SELECT cartodb_id as id, the_geom, spname, splbl, subdist, subsub FROM support_nysp_sd WHERE cartodb_id='${id}'`;
+  return `SELECT cartodb_id as id, the_geom, spname, splbl, subdist, subsub FROM support_zoning_spsd WHERE cartodb_id='${id}'`;
 };
 
 export default DS.JSONAPIAdapter.extend({
