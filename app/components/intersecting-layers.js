c37b1e88c4d49dd133c80bc2a1559b3e7ec85d22
allthesignals <wilburnforce@gmail.com>
2017-12-28 16:05:12 -0500

    Cancel intersecting layers calculations
    Will address some issues in #477, but not solve it.

--- app/components/intersecting-layers.js
+++ app/components/intersecting-layers.js
@@ -48,6 +48,14 @@ export default Ember.Component.extend({
     return yield RSVP.hash(hash);
   }).restartable(),
 
+  willDestroyElement(...args) {
+    this.get('calculateIntersections').cancelAll();
+  },
+
+  willUpdate(...args) {
+    this.get('calculateIntersections').cancelAll();
+  },
+
   @computed('tables.@each', 'bbl', 'responseIdentifier')
   intersectingLayers(...args) {
     return this.get('calculateIntersections').perform(...args);
