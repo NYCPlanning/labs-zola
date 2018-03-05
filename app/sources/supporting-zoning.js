86e28e6fc020790be1fda6760678869943a93bc3
Chris Whong <chris@whong.org>
2018-03-05 13:49:56 -0500

    update edesignations table

--- app/sources/supporting-zoning.js
+++ app/sources/supporting-zoning.js
@@ -56,7 +56,7 @@ export default {
     },
     {
       id: 'e-designations',
-      sql: 'SELECT the_geom_webmercator, bbl, ceqr_num, enumber, ulurp_num FROM e_designations',
+      sql: 'SELECT the_geom_webmercator, bbl, ceqr_num, enumber, ulurp_num FROM e_designations_v20180214',
     },
   ],
 };
