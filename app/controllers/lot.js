af080605270179238aa356266731ff88aafbe74a
Chris Whong <chris@whong.org>
2018-03-06 23:43:06 -0500

    add leading zero to historical zoning map url

--- app/controllers/lot.js
+++ app/controllers/lot.js
@@ -3,4 +3,9 @@ import { computed } from 'ember-decorators/object'; // eslint-disable-line
 import Bookmarkable from '../mixins/bookmarkable';
 
 export default Ember.Controller.extend(Bookmarkable, {
+
+  @computed('lot.zonemap')
+  paddedZonemap(zonemap) {
+    return (`0${zonemap}`).slice(-3);
+  },
 });
