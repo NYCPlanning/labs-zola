c33ce460f68bd9db24b5f5cad702cdc6be272b35
allthesignals <wilburnforce@gmail.com>
2018-07-31 14:30:21 -0400

    Removes unused component

--- tests/integration/components/cascading-checkbox-test.js
+++
@@ -1,8 +0,0 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
-
-module('Integration | Component | cascading checkbox', function(hooks) {
-  setupRenderingTest(hooks);
-});
