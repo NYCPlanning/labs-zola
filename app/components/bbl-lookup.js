c2e3247418546bf39d23bba685dd8d425dd28ca3
Andy Cochran <acochran@planning.nyc.gov>
2018-07-31 14:08:18 -0400

    improve BBL form UI

--- app/components/bbl-lookup.js
+++ app/components/bbl-lookup.js
@@ -20,26 +20,36 @@ export default Component.extend({
   focused: false,
   errorMessage: '',
   closed: true,
+  validBlock: false,
+  validLot: false,
 
   actions: {
+    validate() {
+      const boro = this.get('boro');
+      const block = this.get('block');
+      const lot = this.get('lot');
+
+      const validBoro = (boro !== '');
+      const validBlock = ((block !== '') && (parseInt(block, 10) < 100000) && (parseInt(block, 10) > 0));
+      const validLot = ((lot !== '') && (parseInt(lot, 10) < 10000) && (parseInt(lot, 10) > 0));
+
+      this.send('resetErrorMessage');
+      this.set('validBlock', validBoro && validBlock);
+      this.set('validLot', validBoro && validBlock && validLot);
+    },
+
     checkBBL() {
       const { boro: { code }, block, lot } = this;
 
-      const uniqueSQL = `select bbl from mappluto_v18_1 where block= ${block} and lot = ${lot} and borocode = ${code}`;
-      carto.SQL(uniqueSQL).then((response) => {
-        if (response[0]) {
-          this.set('errorMessage', '');
-          this.setProperties({
-            selected: 0,
-            focused: false,
-            closed: true,
-          });
-
-          this.transitionTo('lot', code, block, lot);
-        } else {
-          this.set('errorMessage', 'The BBL does not exist.');
-        }
-      });
+      if (code && block && lot) {
+        this.send('goToLot');
+      }
+      if (block && !lot) {
+        this.send('goToBlock');
+      }
+      if (!code && !block && !lot) {
+        this.set('errorMessage', 'Please fill out all required fields.');
+      }
     },
 
     resetErrorMessage() {
@@ -48,7 +58,45 @@ export default Component.extend({
 
     setBorocode(option) {
       this.set('boro', option);
-      this.set('errorMessage', '');
+      this.send('validate');
+    },
+
+    goToLot() {
+      const { boro: { code }, block, lot } = this;
+      const SQL = `select bbl from mappluto_v18_1 where borocode = ${code} and block = ${block} and lot = ${lot}`;
+
+      carto.SQL(SQL)
+        .then(([response]) => {
+          if (response) {
+            this.setProperties({
+              closed: true,
+              errorMessage: '',
+            });
+            this.transitionTo('lot', code, block, lot);
+          } else {
+            this.set('errorMessage', 'The BBL does not exist.');
+          }
+        });
+    },
+
+    goToBlock() {
+      const { boro: { code }, block } = this;
+      const { mapInstance } = this.get('mainMap');
+      const SQL = `SELECT the_geom FROM mappluto_block_centroids WHERE block= ${parseInt(block, 10)} AND borocode = ${code}`;
+
+      carto.SQL(SQL, 'geojson').then((response) => {
+        if (response.features[0]) {
+          console.log('response has features');
+          this.set('errorMessage', '');
+          this.setProperties({
+            closed: true,
+          });
+          mapInstance.flyTo(response.features[0].geometry.coordinates, 16);
+        } else {
+          console.log('no features in response');
+          this.set('errorMessage', 'The Block does not exist.');
+        }
+      });
     },
   },
 });
