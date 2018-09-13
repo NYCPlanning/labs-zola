d53ffe4a84ba2551bdcaca5d25ed8db00339fd62
Chris Whong <chris@whong.org>
2018-09-13 17:14:11 -0400

    alias title in layer-group model

--- app/models/layer-group.js
+++ app/models/layer-group.js
@@ -1,7 +1,11 @@
+import DS from 'ember-data';
 import LayerGroupModel from 'ember-mapbox-composer/models/layer-group';
 import { alias } from '@ember/object/computed';
 import QueryParamMap from '../mixins/query-param-map';
 
 export default LayerGroupModel.extend(QueryParamMap, {
   'query-param': alias('id'),
+  title: alias('legend.label'),
+  meta: DS.attr(),
+  legend: DS.attr(),
 });
