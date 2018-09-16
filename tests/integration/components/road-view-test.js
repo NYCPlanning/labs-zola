99e843a76c26d093269ca66b7b3cefd186665ae1
Chris Whong <chris@whong.org>
2018-09-15 20:35:55 -0400

    remove default template block test

--- tests/integration/components/road-view-test.js
+++ tests/integration/components/road-view-test.js
@@ -14,12 +14,4 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#road-view}}
-      template block text
-    {{/road-view}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
