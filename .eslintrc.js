b91ca95918b4e0b7d899f3848c66c02b39b19573
allthesignals <wilburnforce@gmail.com>
2018-10-23 10:39:41 -0400

    Upgrade Ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
       files: [
         'ember-cli-build.js',
         'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
