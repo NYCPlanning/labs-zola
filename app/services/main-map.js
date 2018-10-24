cea2ff41360c0695f6a385221ec2eddc2120b7ef
allthesignals <wilburnforce@gmail.com>
2018-10-24 15:03:15 -0400

    Cleanup services

--- app/services/main-map.js
+++ app/services/main-map.js
@@ -1,22 +1,26 @@
 import Service from '@ember/service';
-import { computed } from 'ember-decorators/object'; // eslint-disable-line
+import { computed } from '@ember-decorators/object';
 import $ from 'jquery';
 import pointLayer from '../layers/point-layer';
 
 const DEFAULT_BOUNDS = [-73.9, 40.690913, -73.832692, 40.856654];
 
-export default Service.extend({
-  mapInstance: null,
+export default class MainMapService extends Service {
+  mapInstance = null;
 
-  // currently selected lot, usually a Lot model
-  selected: null,
-  currentZoom: null,
-  currentMeasurement: null,
-  drawMode: null,
-  shouldFitBounds: false,
+  selected = null;
+
+  currentZoom = null;
+
+  currentMeasurement = null;
+
+  drawMode = null;
+
+  shouldFitBounds = false;
 
   @computed('selected')
-  bounds(selected) {
+  get bounds() {
+    const selected = this.get('selected');
     const { mapInstance } = this;
     if (mapInstance) {
       mapInstance.resize();
@@ -26,24 +30,26 @@ export default Service.extend({
       return selected.get('bounds');
     }
     return DEFAULT_BOUNDS;
-  },
+  }
 
-  pointLayer,
-  currentAddress: null,
+  pointLayer = pointLayer;
 
-  drawnFeature: null,
+  currentAddress = null;
+
+  drawnFeature = null;
 
   @computed('drawnFeature')
-  drawnFeatureSource(feature) {
+  get drawnFeatureSource() {
+    const feature = this.get('drawnFeature');
     return {
       type: 'geojson',
       data: feature,
     };
-  },
-
+  }
 
   @computed('currentAddress')
-  addressSource(currentAddress) {
+  get addressSource() {
+    const currentAddress = this.get('currentAddress');
     return {
       type: 'geojson',
       data: {
@@ -51,10 +57,11 @@ export default Service.extend({
         coordinates: currentAddress,
       },
     };
-  },
+  }
 
   @computed('selected')
-  isSelectedBoundsOptions(selected) {
+  isSelectedBoundsOptions() {
+    const selected = this.get('selected');
     const el = $('.map-container');  // eslint-disable-line
     const height = el.height();
     const width = el.width();
@@ -73,7 +80,7 @@ export default Service.extend({
       padding: selected && (type !== 'zoning-district') && (type !== 'commercial-overlay') ? padding : 0,
       offset: [offset, 0],
     };
-  },
+  }
 
   resetBounds() {
     const { mapInstance } = this;
@@ -81,5 +88,5 @@ export default Service.extend({
       mapInstance.resize();
     }
     this.set('selected', null);
-  },
-});
+  }
+}
