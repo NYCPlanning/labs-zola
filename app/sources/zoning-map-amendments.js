b64225cf212a975f0d68350315e718e109953c77
Hannah Kates <hannah.b.kates@gmail.com>
2018-12-12 16:09:15 -0500

    fix merge conflict

--- app/sources/zoning-map-amendments.js
+++ app/sources/zoning-map-amendments.js
@@ -4,11 +4,7 @@ export default {
   'source-layers': [
     {
       id: 'zoning-map-amendments',
-<<<<<<< HEAD
-      sql: 'SELECT * FROM (SELECT the_geom_webmercator, to_char(effective, \'MM/DD/YYYY\') as effectiveformatted, effective, ulurpno, status, project_na FROM zoning_map_amendments_v201811 WHERE status = \'Adopted\') a',
-=======
       sql: 'SELECT * FROM (SELECT the_geom_webmercator, to_char(effective, \'MM/DD/YYYY\') as effectiveformatted, effective, ulurpno, status, project_na FROM zoning_map_amendments_v20181206 WHERE status = \'Adopted\') a',
->>>>>>> master
     },
   ],
 };
