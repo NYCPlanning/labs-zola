8f191cc99826816060c2de96eb0e1e6e820ea550
allthesignals <wilburnforce@gmail.com>
2019-01-31 17:41:22 -0500

    Apply d617c1423bd3097762f4b5e7c7fcdd728cde29d5

--- tests/helpers/setup-map-mocks.js
+++ tests/helpers/setup-map-mocks.js
@@ -2,7 +2,7 @@ import { registerWaiter, unregisterWaiter } from '@ember/test';
 import LabsMap from 'ember-mapbox-composer/components/labs-map';
 import MapboxGL from 'ember-mapbox-gl/components/mapbox-gl';
 import Service, { inject as service } from '@ember/service';
-import { service as serviceDec } from '@ember-decorators/service';
+import { inject as serviceDec } from '@ember-decorators/service';
 
 export default function(hooks) {
   hooks.beforeEach(async function() {
@@ -46,6 +46,9 @@ export default function(hooks) {
       willDestroyElement(...params) {
         unregisterWaiter(this.testWaiter.bind(this));
 
+        this.map.off('idle', this.callbacks.willIdle);
+        this.map.off('render', this.callbacks.willRender);
+
         this._super(...params);
       }
 
@@ -65,12 +68,21 @@ export default function(hooks) {
       }
 
       _onLoad(map) {
+        // callbacks
+        this.callbacks = {
+          willIdle: () => {
+            this.swapInImage(map);
+            this.didIdle = true;
+          },
+          willRender: () => {
+            this.swapInImage(map);
+          },
+        };
+
         // override maploaded with an event listener that replaces
         // the canvas with an image for testing purposes
-        map.on('idle', () => {
-          this.swapInImage(map);
-          this.didIdle = true;
-        });
+        map.on('idle', this.callbacks.willIdle);
+        map.on('render', this.callbacks.willRender);
 
         super._onLoad(map);
       }
