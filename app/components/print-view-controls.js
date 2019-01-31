5d3edd3d924fce4a8395ecdfac337b2e1e0ed66b
Andy Cochran <acochran@planning.nyc.gov>
2019-01-31 17:05:29 -0500

    work on print controls component

--- app/components/print-view-controls.js
+++ app/components/print-view-controls.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
+import { argument } from '@ember-decorators/argument';
 
 export default class PrintViewControlsComponent extends Component {
-  // classNames: 'print-view--controls',
+  classNames = ['print-view--controls'];
+
+  @argument
+  printViewOrientation;
+
+  @argument
+  printViewPaperSize;
 }
