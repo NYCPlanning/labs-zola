b6a9f410f2e6c4fa59c46f0b4ee0c81073e8bfed
Hannah Kates <hannah.b.kates@gmail.com>
2019-02-05 12:00:42 -0500

    764: use aliased views for new jan datasets (#769)

--- tests/integration/helpers/get-unique-options-for-test.js
+++ tests/integration/helpers/get-unique-options-for-test.js
@@ -9,7 +9,7 @@ module('helper:get-unique-options-for', function(hooks) {
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
-    this.set('sql', 'SELECT * FROM commercial_overlays_v20181206');
+    this.set('sql', 'SELECT * FROM commercial_overlays');
 
     await render(hbs`{{get-unique-options-for 'overlay' sql}}`);
 
