1dc36b0239af2efaa9c39813acdf17b65a26b208
Matt Gardner <wilburnforce@gmail.com>
2019-02-06 13:43:38 -0500

    761 Add tests for current working behavior (#775)
    761 Test behaviors for selected overlays
    
    761 add tests for capturing status quo behavior of QPs
    
    Fix lint
    
    761 failing test
    
    Adds a failing test proving the regression behavior
    
    761 Implement fix; add additional tests

---
+++ tests/helpers/extract-query-params.js
@@ -0,0 +1,17 @@
+export default function(URLStringFromCurrentURL) {
+  const URLParsed = new URL(`http://localhost${URLStringFromCurrentURL}`);
+  const SearchParams = new URLSearchParams(URLParsed.search);
+
+  const queryParams = {};
+
+  SearchParams.forEach((value, key) => {
+    try {
+      queryParams[key] = JSON.parse(value);
+    } catch (e) {
+      console.warn(`${key} did not parse for test - skipping.`);
+      queryParams[key] = value;
+    }
+  });
+
+  return queryParams;
+}
