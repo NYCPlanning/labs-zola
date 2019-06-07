c32eca46cc4d0903f2fc7d57988b47d6d8a3efb0
allthesignals <wilburnforce@gmail.com>
2019-06-07 18:03:40 -0400

    847 retry findRecord for lots; clean up serializer; remove blocking initializer

--- app/routes/lot.js
+++ app/routes/lot.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 import bblDemux from '../utils/bbl-demux';
 import updateSelectionSingleFeatureMixin from '../mixins/update-selection-single-feature';
 
@@ -10,7 +11,19 @@ export default Route.extend(updateSelectionSingleFeatureMixin, {
     const id = bblDemux(params);
 
     return {
-      taskInstance: this.store.findRecord('lot', id),
+      taskInstance: this.retryableFindLot.perform(id),
     };
   },
+
+  retryableFindLot: task(function* (id) {
+    try {
+      return yield this.store.findRecord('lot', id);
+    } catch (e) {
+      if (e.errors[0] === 'query_timeout_exceeded') {
+        yield this.retryableFindLot.perform(id);
+      }
+    }
+
+    return null;
+  }),
 });
--- tests/unit/routes/lot-test.js
+++ tests/unit/routes/lot-test.js
@@ -1,11 +1,52 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Unit | Route | lot', function(hooks) {
   setupTest(hooks);
+  setupMirage(hooks);
 
   test('it exists', function (assert) {
     const route = this.owner.lookup('route:lot');
     assert.ok(route);
+  });
+
+  test('it fetches data', async function (assert) {
+    const route = this.owner.lookup('route:lot');
+
+    const { taskInstance } = await route.model({
+      boro: 1,
+      block: 1,
+      lot: 1,
+    });
+
+    await taskInstance;
+
+    assert.ok(taskInstance.value);
+  });
+
+  test('it retries after a query timeout', async function(assert) {
+    let timesCalled = 0;
+    assert.expect(4);
+
+    this.server.get('https://planninglabs.carto.com/api/v2/sql', (schema) => {
+      assert.ok(true);
+
+      timesCalled += 1;
+
+      if (timesCalled > 3) {
+        return schema.lots.first();
+      }
+
+      return { errors: ['query_timeout_exceeded'] };
+    }, 500);
+
+    const route = this.owner.lookup('route:lot');
+
+    await route.model({
+      boro: 1,
+      block: 1,
+      lot: 1,
+    });
   });
 });
