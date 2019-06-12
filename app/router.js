1afcd0cadb924a27d700b27a6be142a8557405f4
allthesignals <wilburnforce@gmail.com>
2019-06-12 18:59:27 -0400

    legacy redirects; incls lots

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = EmberRouter.extend(trackPage, {
 
 Router.map(function () {// eslint-disable-line
   // first class layer group routes
-  this.route('lot', { path: 'lot/:boro/:block/:lot' });
+
 
   // supplementary navigation routes
   this.route('bbox', { path: 'bbox/:west/:south/:east/:north' });
@@ -28,9 +28,18 @@ Router.map(function () {// eslint-disable-line
     this.route('special-purpose-district', { path: '/special-purpose-district/:id' });
     this.route('special-purpose-subdistrict', { path: '/special-purpose-subdistrict/:id' });
     this.route('zoning-map-amendment', { path: '/zma/:id' });
+    this.route('lot', { path: 'lot/:boro/:block/:lot' });
   });
 
-  this.route('legacy-redirects', { path: '/:type/:option_a' });
+  // in order to namespace the map feature routes (zoning district, etc), we have
+  // to preserve functionality of old hyperlinks. this route redirects old
+  // hyperlinks to to the new route, which is namespaced with `/l`
+  this.route('legacy-redirects', { path: '/commercial-overlay/:option_a' });
+  this.route('legacy-redirects', { path: '/zoning-district/:option_a' });
+  this.route('legacy-redirects', { path: '/special-purpose-district/:id' });
+  this.route('legacy-redirects', { path: '/special-purpose-subdistrict/:id' });
+  this.route('legacy-redirects', { path: '/zoning-map-amendment/:id' });
+  this.route('legacy-redirects', { path: '/lot/:boro/:block/:lot' });
 });
 
 export default Router;
