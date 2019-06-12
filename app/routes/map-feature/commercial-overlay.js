cfc0a93e6248613e57bfd50f522284f0520a3dfb
allthesignals <wilburnforce@gmail.com>
2019-06-12 17:35:34 -0400

    commercial overlay

--- app/routes/map-feature/commercial-overlay.js
+++ app/routes/map-feature/commercial-overlay.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-});
+const cartoQueryTemplate = function(id) {
+  return `SELECT * FROM (
+    SELECT ST_CollectionExtract(ST_Collect(the_geom),3) as the_geom, overlay as id, overlay FROM commercial_overlays GROUP BY overlay
+  ) a WHERE id='${id}'`;
+};
+
+export default class CommercialOverlayRoute extends Route {
+  model(params) {
+    const { id } = params;
+    const cartoQuery = cartoQueryTemplate(id);
+
+    return {
+      cartoQuery,
+    };
+  }
+}
