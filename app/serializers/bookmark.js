0d7411329a6aa051ff29c174f175c44318df0293
allthesignals <wilburnforce@gmail.com>
2019-06-27 02:01:20 -0400

    Hotfix legacy model name

--- app/serializers/bookmark.js
+++ app/serializers/bookmark.js
@@ -1,1 +1,16 @@
-export { default } from 'ember-local-storage/serializers/serializer';
+import LocalStorageSerializer from 'ember-local-storage/serializers/serializer';
+import { get } from '@ember/object';
+
+export default LocalStorageSerializer.extend({
+  normalizeFindAllResponse(store, primaryModelClass, payload, ...args) {
+    payload.data.forEach((record) => {
+      if (record.relationships) {
+        if (get(record, 'relationships.bookmark.data.type') === 'zmas') {
+          record.relationships.bookmark.data.type = 'zoning-map-amendment';
+        }
+      }
+    });
+
+    return this._super(store, primaryModelClass, payload, ...args);
+  },
+});
