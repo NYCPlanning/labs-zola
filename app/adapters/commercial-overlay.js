d5fcaed0a463c9bc1eec3fce0a3c2c43736352ce
Tyler Matteo <tylermatteo434@gmail.com>
2021-12-06 13:33:55 -0500

    Updating labs-ember-search to version with correct sql query and fixing a bunch of linting violations

--- app/adapters/commercial-overlay.js
+++ app/adapters/commercial-overlay.js
@@ -2,7 +2,7 @@ import { buildSqlUrl } from '../utils/carto';
 import CartoGeojsonFeatureAdapter from './carto-geojson-feature';
 
 const SQL = function(id) {
-    return `SELECT * FROM (
+  return `SELECT * FROM (
     SELECT ST_CollectionExtract(ST_Collect(the_geom),3) as the_geom,
       overlay as id,
       overlay
@@ -12,10 +12,10 @@ const SQL = function(id) {
 };
 
 export default CartoGeojsonFeatureAdapter.extend({
-    urlForFindRecord(id) {
-        return buildSqlUrl(
-            SQL(id),
-            'geojson',
-        );
-    },
+  urlForFindRecord(id) {
+    return buildSqlUrl(
+      SQL(id),
+      'geojson',
+    );
+  },
 });
