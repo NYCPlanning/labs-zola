9484de26a372f7e21329fab054dd2e0160283c86
Matt Gardner <wilburnforce@gmail.com>
2022-07-05 10:57:19 -0500

    Temporarily allow dep lint versions

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
+  allowedVersions: {
+   // TODO: Fix these dependency issues
+    '@fortawesome/ember-fontawesome': '0.1.14 || 0.1.13',
 
+   // TODO: Fix these dependency issues
+    'ember-inflector': '3.0.0 || 3.0.1',
+
+   // TODO: Fix these dependency issues
+    'ember-data': '3.12.6 || 3.10.0',
+  },
 };
