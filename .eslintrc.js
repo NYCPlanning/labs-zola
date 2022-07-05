89a8717f7920256b7477a6621a06670db57510e1
Matt Gardner <wilburnforce@gmail.com>
2022-07-05 10:17:54 -0500

    3.12

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,8 +52,7 @@ module.exports = {
         'lib/*/index.js'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
