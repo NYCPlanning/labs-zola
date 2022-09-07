a63b5217148f47b825a5bfb1af45eda4aab3d320
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2022-09-07 17:02:49 -0400

    Updating linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     'no-param-reassign': 0,
     'ember/avoid-leaking-state-in-ember-objects': 0,
     'ember-best-practices/require-dependent-keys': 0,
+    'no-undef': 0,
   },
   overrides: [
     // node files
