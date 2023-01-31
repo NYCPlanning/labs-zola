f9384b1e725de0b677076fbb47c7b86fc7f4e6e4
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-01-31 10:25:28 -0500

    ULURP number links to ZAP Search page for appropriate project

--- app/routes/map-feature/zoning-map-amendment.js
+++ app/routes/map-feature/zoning-map-amendment.js
@@ -1,13 +1,20 @@
 import Route from '@ember/routing/route';
+import fetch from 'node-fetch';
+import config from 'labs-zola/config/environment';
 
 export default class ZoningDistrictRoute extends Route {
-  model(params) {
+  async model(params) {
     const { id } = params;
     const { search } = this.paramsFor('map-feature');
+
+    const response = await fetch(`${config.zapApiHost}/projects?action-ulurpnumber[]=${id}`);
+    const ulurp = await response.json();
+    const zapId = (ulurp.data.length === 1) ? ulurp.data[0].id : null;
 
     return {
       id,
       search,
+      zapId,
     };
   }
 }
