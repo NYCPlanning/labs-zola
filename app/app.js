bcf231a798c58287484b888ea44ed08c423d1576
Matt Gardner <wilburnforce@gmail.com>
2023-02-18 17:34:14 -0500

    Drop workaround for regsiterWaiter

--- app/app.js
+++ app/app.js
@@ -25,18 +25,6 @@ export default class App extends Application {
   Resolver = Resolver;
 }
 
-// temporary fix due to importing registerWaiter
-// see: https://github.com/emberjs/ember.js/issues/15670
-/* eslint-disable ember-suave/no-direct-property-access, no-undef */
-if (typeof Ember.Test === 'undefined') {
-  Ember.Test = {
-    registerWaiter() {
-
-    },
-  };
-}
-/* eslint-enable ember-suave/no-direct-property-access, no-undef */
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 loadInitializers(App, config.modulePrefix);
