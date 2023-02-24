8df9fd38ca1d7c3eb7729175c1bf34b8954e5236
Matt Gardner <wilburnforce@gmail.com>
2023-02-23 20:15:06 -0500

    Remove prember, use dummy fastboot service

---
+++ tests/unit/services/fastboot-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | fastboot', function(hooks) {
+  setupTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it exists', function(assert) {
+    const service = this.owner.lookup('service:fastboot');
+    assert.ok(service);
+  });
+});
