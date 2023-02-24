427a97052d88b9f860ccab2422f618ee8a789828
Matt Gardner <wilburnforce@gmail.com>
2023-02-23 20:06:41 -0500

    Drop sentry

--- app/app.js
+++ app/app.js
@@ -1,18 +1,8 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
 import defineModifier from 'ember-concurrency-retryable/define-modifier';
 import config from 'labs-zola/config/environment';
-
-if (config.environment === 'production' || config.environment === 'staging') {
-  Sentry.init({
-    dsn: 'https://f93ba4c5c59740c4b70cdb571b54d6da@sentry.io/1492094',
-    integrations: [new Integrations.Ember()],
-    environment: config.environment,
-  });
-}
 
 // necessary for applying e-concurrency extensions
 // to their decorators
