65cbcc2697d7ae7ad5f76ce731a34c70b4163bda
Tyler Matteo <tylermatteo434@gmail.com>
2023-07-13 16:12:45 -0400

    Move icon config to icons.js file

--- config/environment.js
+++ config/environment.js
@@ -307,44 +307,6 @@ module.exports = function(environment) {
       domain: `https://${CARTO_USER}.carto.com`,
     },
 
-    fontawesome: {
-      icons: {
-        'free-regular-svg-icons': [
-          'check-square',
-          'circle',
-          'dot-circle',
-          'square',
-        ],
-        'free-solid-svg-icons': [
-          'angle-up',
-          'bookmark',
-          'caret-down',
-          'caret-up',
-          'check-square',
-          'chevron-down',
-          'chevron-left',
-          'chevron-up',
-          'circle',
-          'exclamation-triangle',
-          'external-link-alt',
-          'home',
-          'info-circle',
-          'link',
-          'map-pin',
-          'minus',
-          'pen',
-          'print',
-          'search',
-          'spinner',
-          'square',
-          'times',
-          'tree',
-          'undo',
-          'window-minimize',
-        ],
-      },
-    },
-
     metricsAdapters: [
       {
         name: 'GoogleAnalytics',
---
+++ config/icons.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function() {
+  return {
+    'free-regular-svg-icons': [
+      'check-square',
+      'circle',
+      'dot-circle',
+      'square',
+    ],
+    'free-solid-svg-icons': [
+      'angle-up',
+      'bookmark',
+      'caret-down',
+      'caret-up',
+      'check-square',
+      'chevron-down',
+      'chevron-left',
+      'chevron-up',
+      'circle',
+      'exclamation-triangle',
+      'external-link-alt',
+      'home',
+      'info-circle',
+      'link',
+      'map-pin',
+      'minus',
+      'pen',
+      'print',
+      'search',
+      'spinner',
+      'square',
+      'times',
+      'tree',
+      'undo',
+      'window-minimize',
+    ],
+  };
+};
