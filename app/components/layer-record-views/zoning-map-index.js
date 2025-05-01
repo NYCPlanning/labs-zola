a4421041676b2a468a6c8f8ee98683e4b9d281de
David Hochbaum <dhochbaum@planning.nyc.gov>
2025-05-01 18:03:31 -0400

    String manipulation of header text.

--- app/components/layer-record-views/zoning-map-index.js
+++ app/components/layer-record-views/zoning-map-index.js
@@ -1,3 +1,9 @@
 import LayerRecordComponent from './-base';
 
-export default class ZoningMapIndexRecordComponent extends LayerRecordComponent {}
+export default class ZoningMapIndexRecordComponent extends LayerRecordComponent {
+  get displayName() {
+    return this.model.id.length > 3
+      ? `SUB PLAN OF ${this.model.id.slice(0, 3)}`
+      : this.model.id;
+  }
+}
