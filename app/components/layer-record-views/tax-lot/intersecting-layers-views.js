39d60dd71bb5b65e5e7dfd610bd5f8d8986a989b
horatio <horatiorosa@gmail.com>
2026-06-10 11:18:23 -0400

    updated zola queries to enable correct highlighting

--- app/components/layer-record-views/tax-lot/intersecting-layers-views.js
+++ app/components/layer-record-views/tax-lot/intersecting-layers-views.js
@@ -21,6 +21,6 @@ export default class IntersectingLayersViews extends Component {
     'upland_waterfront_areas',
     'transitzones',
     'appendixj_designated_mdistricts',
-    'mta_rail_station_50ft_buffers',
+    'mta_nyc_rail_station_buffer_dissolve',
   ];
 }
