76d98cc133532b54fec9e16f0e486090c848246d
pratishta <ypradhi9@gmail.com>
2026-07-27 16:50:59 -0400

    Switch to carto caching server domain
    Remove logging
    
    Remove linting changes
    
    Update to staging layers-api
    
    Remove backslash in url
    
    Use data-qa layers-api

--- mirage/config.js
+++ mirage/config.js
@@ -14,9 +14,11 @@ function routes() {
     'https://labs-mapbox-gl-noop-tiles.nyc3.digitaloceanspaces.com/**'
   );
 
-  this.get('https://planninglabs.carto.com/api/v2/sql', (schema, request) => {
-    const { queryParams } = request;
-    const { format, q } = queryParams;
+  this.get(
+    'https://carto.nycplanningdigital.com/api/v2/sql',
+    (schema, request) => {
+      const { queryParams } = request;
+      const { format, q } = queryParams;
 
     // by default, this will return a feature that looks like a PLUTO Lot
     if (format === 'geojson') {
@@ -29,33 +31,34 @@ function routes() {
 
       let { models: features } = schemaModel;
 
-      // NOTE: some tests will load the Mirage mock server with multiple fake carto responses. This leads
-      // to multiple records being included with the response, something that should never happen in
-      // production. This means that when it gets back to ember, the request includes an id but receives
-      // a different record. In the past, this wouldn't be an issue in Ember Data. Now, it seems to throw an
-      // error.
-      // As a fix, this code looks for a key:value pair in the request query that's sent to carto: id:{id}". Then,
-      // it filters for the correct record.
-      // The reason it's stored as a comment is that pretender or mirage is truncating everything after the "="
-      if (features.length > 1) {
-        try {
-          const regex = /([^, ]+):([^, ]+)/g;
-          const found = q.match(regex);
-          const cartoIdentifier = found[0]?.split(':')[1];
-          features = features.filter((f) => f.id === cartoIdentifier);
-        } catch (e) {
-          console.error('Mirage error: ', e);
+        // NOTE: some tests will load the Mirage mock server with multiple fake carto responses. This leads
+        // to multiple records being included with the response, something that should never happen in
+        // production. This means that when it gets back to ember, the request includes an id but receives
+        // a different record. In the past, this wouldn't be an issue in Ember Data. Now, it seems to throw an
+        // error.
+        // As a fix, this code looks for a key:value pair in the request query that's sent to carto: id:{id}". Then,
+        // it filters for the correct record.
+        // The reason it's stored as a comment is that pretender or mirage is truncating everything after the "="
+        if (features.length > 1) {
+          try {
+            const regex = /([^, ]+):([^, ]+)/g;
+            const found = q.match(regex);
+            const cartoIdentifier = found[0]?.split(':')[1];
+            features = features.filter((f) => f.id === cartoIdentifier);
+          } catch (e) {
+            console.error('Mirage error: ', e);
+          }
         }
+
+        return {
+          type: 'FeatureCollection',
+          features,
+        };
       }
 
-      return {
-        type: 'FeatureCollection',
-        features,
-      };
+      return { rows: [] };
     }
-
-    return { rows: [] };
-  });
+  );
 
   this.namespace = '/v1';
   this.get('layer-groups');
